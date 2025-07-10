9b8ae1f30fc9449cc380809b5c4dec85052f6685
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2025-07-10 00:17:41 +0000

    fix: localhost origin

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ import { getRoomsRoute } from './http/routes/get-rooms.ts';
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
-  origin: 'http://localhost:5173',
+  origin: 'http://127.0.0.1:5173',
 });
 
 app.setSerializerCompiler(serializerCompiler);
