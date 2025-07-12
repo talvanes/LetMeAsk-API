4637d3a704ea6e49994c08516b283972483c566b
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2025-07-12 13:44:20 +0000

    feat: add audio upload feature

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { fastifyCors } from '@fastify/cors';
+import {fastifyMultipart} from '@fastify/multipart'
 import { fastify } from 'fastify';
 import {
   serializerCompiler,
@@ -11,12 +12,15 @@ import { createQuestionRoute } from './http/routes/create-question.ts';
 import { createRoomRoute } from './http/routes/create-room.ts';
 import { getRoomsQuestionsRoute } from './http/routes/get-room-questions.ts';
 import { getRoomsRoute } from './http/routes/get-rooms.ts';
+import { uploadAudioRoute } from './http/routes/upload-audio.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
   origin: 'http://127.0.0.1:5173',
 });
+
+app.register(fastifyMultipart)
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
@@ -29,5 +33,6 @@ app.register(getRoomsRoute);
 app.register(createRoomRoute);
 app.register(getRoomsQuestionsRoute);
 app.register(createQuestionRoute);
+app.register(uploadAudioRoute);
 
 app.listen({ port: env.PORT });
