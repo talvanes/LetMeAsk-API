de590b9330546a2eedec8e1a2c33add422b66697
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2025-07-12 17:03:21 +0000

    feat: generate answers for questions

--- src/http/routes/create-question.ts
+++ src/http/routes/create-question.ts
@@ -1,7 +1,9 @@
-import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod';
-import { z } from 'zod/v4';
-import { db } from '../../db/connection.ts';
-import { schema } from '../../db/schema/index.ts';
+import { and, eq, sql } from 'drizzle-orm'
+import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
+import { z } from 'zod/v4'
+import { db } from '../../db/connection.ts'
+import { schema } from '../../db/schema/index.ts'
+import { generateAnswer, generateEmbeddings } from '../../services/gemini.ts'
 
 export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
   app.post(
@@ -17,22 +19,55 @@ export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
       },
     },
     async ({ body, params }, reply) => {
-      const { question } = body;
-      const { roomId } = params;
+      const { question } = body
+      const { roomId } = params
+
+      const embeddings = await generateEmbeddings(question)
+      const embeddingsAsString = `[${embeddings.join(',')}]`
+
+      const chunks = await db
+        .select({
+          id: schema.audioChunks.id,
+          transcription: schema.audioChunks.transcription,
+          similarity: sql<number>`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector)`,
+        })
+        .from(schema.audioChunks)
+        .where(
+          and(
+            eq(schema.audioChunks.roomId, roomId),
+            sql`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector) > 0.7`
+          )
+        )
+        .orderBy(
+          sql`${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector`
+        )
+        .limit(3)
+
+      let answer: string | null = null
+
+      if (chunks.length > 0) {
+        const transcriptions = chunks.map((chunk) => chunk.transcription)
+
+        answer = await generateAnswer(question, transcriptions)
+      }
 
       const [insertedQuestion] = await db
         .insert(schema.questions)
         .values({
-            question,
-            roomId,
+          question,
+          roomId,
+          answer,
         })
-        .returning();
+        .returning()
 
       if (!insertedQuestion) {
-        throw new Error('Failed to create new question');
+        throw new Error('Failed to create new question')
       }
 
-      return reply.status(201).send({ questionId: insertedQuestion.id });
+      return reply.status(201).send({
+        questionId: insertedQuestion.id,
+        answer,
+      })
     }
-  );
-};
+  )
+}
